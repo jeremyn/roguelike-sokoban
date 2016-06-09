ddd569d21a9088fe7dc9296997968f821c1779f4
Jeremy Nation <jeremy@jeremynation.me>
2016-06-08 20:02:39 -0400

    Remove print_wrap

--- rlsokoban.py
+++ rlsokoban.py
@@ -4,24 +4,10 @@
 # Released under the GPLv3. See included LICENSE file.
 import argparse
 
-import sys
 import textwrap
-import fcntl
-import termios
-import struct
 import curses
 import src
 import src.constants as const
-
-
-def print_wrap(text, length=None):
-    if length is None:
-        struct_in = struct.pack("HHHH", 0, 0, 0, 0)
-        struct_out = fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, struct_in)
-        term_dimensions = struct.unpack("HHHH", struct_out)
-        length = term_dimensions[1] - 1
-    length = min(length, 79)
-    print(textwrap.fill(text, length))
 
 
 if __name__ == "__main__":
@@ -40,46 +26,68 @@ if __name__ == "__main__":
     try:
         curses.wrapper(src.main, args.level_filename)
     except KeyboardInterrupt:
-        print_wrap("Exiting at user request. Thanks for playing!")
+        final_msg = 'Exiting at user request. Thanks for playing!'
     except src.display.WindowTooSmallError:
-        print_wrap("Error: window too small. Please increase your terminal "
-                   "size to at least 80x24 and try again.")
-    except src.levelloader.MalformedLevelFileError as msg:
-        print_wrap("Problem with contents of level file: %s" % str(msg))
-        print_wrap("")
-        print_wrap("This error means there is something wrong with a custom "
-                   "level file you are trying to use. Please correct that "
-                   "file as needed using the hints in the included level "
-                   "file \'%s\' and try again." %
-                   const.DEFAULT_LEVEL_FILE_NAME)
-    except src.highscores.CorruptHighScoreFileError as msg:
-        print_wrap("Problem with contents of high score file: %s" % str(msg))
-        print_wrap("")
-        print_wrap("This error means your high score file \'%s\' is "
-                   "corrupt. Manually modifying the high score file can "
-                   "leave it in a corrupt state. If there are any \'%s\' "
-                   "files in the main game directory, you can try renaming "
-                   "them to \'%s\' and restarting the game. This may load "
-                   "good (older) data. Otherwise, you will need to remove "
-                   "your existing \'%s\' file so the game can create a fresh "
-                   "file. If you think this problem is the result of a "
-                   "software bug, please report this on the %s issue tracker "
-                   "at" % (const.DEFAULT_HIGH_SCORE_FILE_NAME_FULL,
-                           const.HIGH_SCORE_EXTENSION,
-                           const.DEFAULT_HIGH_SCORE_FILE_NAME,
-                           const.DEFAULT_HIGH_SCORE_FILE_NAME,
-                           const.GAME_NAME))
-        print_wrap("")
-        print_wrap(const.ISSUE_TRACKER)
-        print_wrap("")
-        print_wrap("and save the corrupt high score file for later debugging.")
-    except src.highscores.HighScoreFileHandlingError as msg:
-        print_wrap("High score file error: %s" % str(msg))
-        print_wrap("")
-        print_wrap("Check file permissions and other possible file-related "
-                   "issues and try again.")
-    except src.levelloader.LevelFileHandlingError as msg:
-        print_wrap("Level file error: %s" % str(msg))
-        print_wrap("")
-        print_wrap("Check file permissions and other possible file-related "
-                   "issues and try again.")
+        final_msg = """
+            Error: window too small. Please increase your terminal size to at
+            least 80x24 and try again.
+        """
+    except src.levelloader.MalformedLevelFileError as e:
+        final_msg = """
+            Problem with contents of level file: %(error_msg)s
+
+            This error means there is something wrong with a custom level file
+            you are trying to use. Please correct that file as needed using
+            the hints in the included level file '%(level_filename)s' and try
+            again.
+        """ % {
+            'error_msg': str(e),
+            'level_filename': const.DEFAULT_LEVEL_FILE_NAME,
+        }
+    except src.highscores.CorruptHighScoreFileError as e:
+        final_msg = """
+            Problem with contents of high score file: %(error_msg)s
+
+            This error means your high score file '%(full_filename)s' is
+            corrupt. Manually modifying the high score file can leave it in a
+            corrupt state. If there are any '%(filename_extension)s' files in
+            the main game directory, you can try renaming them to
+            '%(filename)s' and restarting the game. This may load good (older)
+            data. Otherwise, you will need to remove your existing
+            '%(filename)s' file so the game can create a fresh file. If you
+            think this problem is the result of a software bug, please report
+            this on the %(game_name)s issue tracker at
+
+            %(issue_tracker)s
+
+            and save the corrupt high score file for later debugging.
+        """ % {
+            'error_msg': str(e),
+            'filename': const.DEFAULT_HIGH_SCORE_FILE_NAME,
+            'filename_extension': const.HIGH_SCORE_EXTENSION,
+            'full_filename': const.DEFAULT_HIGH_SCORE_FILE_NAME_FULL,
+            'game_name': const.GAME_NAME,
+            'issue_tracker': const.ISSUE_TRACKER,
+        }
+    except src.highscores.HighScoreFileHandlingError as e:
+        final_msg = """
+            High score file error: %(error_msg)s
+
+            Check file permissions and other possible file-related issues and
+            try again.
+        """ % {
+            'error_msg': str(e),
+        }
+    except src.levelloader.LevelFileHandlingError as e:
+        final_msg = """
+            Level file error: %(error_msg)s
+
+            Check file permissions and other possible file-related issues and
+            try again.
+        """ % {
+            'error_msg': str(e),
+        }
+
+    lines = str.splitlines(final_msg)
+    for line in lines:
+        print(textwrap.fill(line.strip()))
